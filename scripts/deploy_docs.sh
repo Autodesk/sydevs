d3d5a9e537d654038d2179ebe1a9daa5ae3402bc
Simon Breslav <simon.breslav@autodesk.com>
2018-04-09 16:08:52 -0400

    small tweaks to the script to show output

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Based on https://voorhoede.github.io/front-end-tooling-recipes/travis-deploy-to-gh-pages/
-# https://github.com/voorhoede/front-end-tooling-recipes/blob/master/travis-deploy-to-gh-pages/scripts/deploy.sh
 
 set -e # exit with nonzero exit code if anything fails
 
@@ -15,7 +14,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 #using token clone gh-pages branch
-git clone --quiet --branch=gh-pages https://${GITHUB_OAUTH_TOKEN}@github.com/Autodesk/sydevs.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GITHUB_OAUTH_TOKEN}@github.com/Autodesk/sydevs.git gh-pages
 
 #go into directory and copy data we're interested in to that directory
 cd gh-pages
@@ -24,6 +23,6 @@ cp -Rf $HOME/doc/html/* ./doc/html
 #add, commit and push files
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin gh-pages > /dev/null
+git push -fq origin gh-pages
 
 echo "Done updating gh-pages\n"
